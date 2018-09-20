b25daa8ceb7f68e2298555234ab2c36fba6b237a
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2018-09-20 16:47:44 +0900

    nullを割り当て可能な値型

---
+++ Util/Nullable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Util
+{
+    [Serializable, StructLayout(LayoutKind.Sequential)]
+    public struct Nullable<T> where T : struct
+    {
+        // These 2 fields represent the state
+        private Boolean hasValue; // Assume null
+        internal T value; // Assume all bits zero
+        public Nullable(T value)
+        {
+            this.value = value;
+            this.hasValue = true;
+        }
+        public Boolean HasValue { get { return hasValue; } }
+        public T Value
+        {
+            get
+            {
+                if (!hasValue)
+                {
+                    throw new InvalidOperationException(
+                    "Nullable object must have a value.");
+                }
+                return value;
+            }
+        }
+        public T GetValueOrDefault() { return value; }
+        public T GetValueOrDefault(T defaultValue)
+        {
+            if (!HasValue) return defaultValue;
+            return value;
+        }
+        public override Boolean Equals(Object other)
+        {
+            if (!HasValue) return (other == null);
+            if (other == null) return false;
+            return value.Equals(other);
+        }
+        public override int GetHashCode()
+        {
+            if (!HasValue) return 0;
+            return value.GetHashCode();
+        }
+        public override string ToString()
+        {
+            if (!HasValue) return "";
+            return value.ToString();
+        }
+        public static implicit operator Nullable<T>(T value)
+        {
+            return new Nullable<T>(value);
+        }
+        public static explicit operator T(Nullable<T> value)
+        {
+            return value.Value;
+        }
+    }
+}
