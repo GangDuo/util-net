e89a2bb5b0dccafc886bd45244e8a285791a818b
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2018-09-20 14:47:31 +0900

    Collectionを指定単位で分割する

---
+++ Util/Chunk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Util
+{
+    /**
+     * Collectionをある数ごとに分割する
+     */
+    public class Chunk<T>
+    {
+        public int ChunkSize { get; set; }
+        private IEnumerable<T> Sources;
+        public Chunk(IEnumerable<T> sources)
+        {
+            this.Sources = sources;
+            this.ChunkSize = 5;
+        }
+        public IEnumerable<IEnumerable<T>> Read()
+        {
+            return this.Sources.Select((entity, index) => new { Index = index, Entity = entity })
+                     .GroupBy(x => x.Index / ChunkSize)
+                     .Select(gr => gr.Select(x => x.Entity));
+        }
+    }
+}
