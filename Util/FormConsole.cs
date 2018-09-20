095bc4c8d12020967b232f87bd6b58d8f7ebec26
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2018-09-20 14:32:24 +0900

    formアプリケーションでコンソール使える

---
+++ Util/FormConsole.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Util
+{
+    public sealed class FormConsole
+    {
+        private static readonly FormConsole _Instance = new FormConsole();
+
+        private class Win32
+        {
+            /// <summary>
+            /// Allocates a new console for current process.
+            /// </summary>
+            [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+            public static extern Boolean AllocConsole();
+
+            /// <summary>
+            /// Frees the console.
+            /// </summary>
+            [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+            public static extern Boolean FreeConsole();
+        }
+
+        private FormConsole()
+        {
+            Win32.AllocConsole();  //Allocates a new console for current process.
+        }
+
+        public static FormConsole Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+
+        ~FormConsole()
+        {
+            Win32.FreeConsole(); // Free the console.
+        }
+    }
+}
